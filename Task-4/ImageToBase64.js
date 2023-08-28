6a27de3971887328e0db9d01d29cc8f2bdd32ca9
Annu <annu.netweb@gmail.com>
2023-08-28 18:55:50 +0530

    Ongoing

---
+++ Task-4/ImageToBase64.js
@@ -0,0 +1,18 @@
+const outputDiv = document.getElementById('outputDiv');
+const inputImage = document.getElementById('inputImage');
+function convertImageToBase64(){
+    
+    if (inputImage.files) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            const base64Image = e.target.result;
+            outputDiv.innerHTML = `Base64 Representation: <pre>${base64Image}</pre>`;
+        }
+
+        reader.readAsDataURL(inputImage.files[0]);
+    } else {
+        outputDiv.innerHTML = "Please select an image.";
+    }
+}
+
