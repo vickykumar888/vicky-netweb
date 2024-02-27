f90e97ecaf3654f4f195d26cfe7577e842b93429
dinesh <dinesh2.netweb@gmail.com>
2024-02-27 11:32:39 +0530

    have done reverse string as directed

--- reverseString.js
+++ reverseString.js
@@ -1,8 +1,22 @@
 // function to reverse a string 
 
-function reverseStr(str){
-    let reverse = str.split("").reverse().join("");
-    return reverse
+// function reverseStr(str){
+//     let reverse = str.split("").reverse().join("");
+//     return reverse
+// }
+// console.log( reverseStr("Hello World"));
+// console.log( reverseStr("Madam"));
+
+// or
+
+function revstr(str){
+    var newString = " ";
+    for (var i = str.length -1; i >= 0; i--){
+        //console.log(i)
+        newString += str[i];
+    }
+  return newString;
 }
-console.log( reverseStr("Hello World"));
-console.log( reverseStr("Madam"));
+console.log(revstr('hello'));
+console.log(revstr('Madam'));
+console.log(revstr('Honey'));
