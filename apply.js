b770c677cf24a8d98960ec2123be4ef995991a9d
dinesh <dinesh2.netweb@gmail.com>
2024-02-27 19:05:44 +0530

    task comlete

--- apply.js
+++ apply.js
@@ -22,3 +22,28 @@ console.log(min);
 
 // console.log(obj.info.apply(obj2 , ["meow"] ))
 
+
+function sum() {
+    let total = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      total += arguments[i];
+    }
+    return total;
+  }
+  const numbers = [1, 2, 3, 4, 5];
+  const result = sum.apply(null, numbers);
+  const result1 = sum.call(null ,  numbers);
+  console.log(result1); // Output: 01,2,3,4,5
+  console.log(result); // Output: 15
+
+// other way of function 
+// function sum() {
+//     let arguments = [1, 2, 3, 4, 5];
+//     let total = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//      total += arguments[i];
+//     }
+//     return total;
+   
+// }
+//  console.log(sum())
