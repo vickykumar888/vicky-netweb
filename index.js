1e243ea4630e5ffe36fb910ffb73af37226d1f04
Nidhi Rana <nidhi2.netweb@gamil.com>
2024-02-28 18:57:17 +0530

    date-time-format

--- index.js
+++ index.js
@@ -1,45 +1,74 @@
-console.log("my first cosnole in js");
-// var, let, const 
+let date = new Date();
+const currentDate = date.getDate();
+const currentMonth = date.getMonth() + 1
+const currentYear = date.getFullYear()
 
-// reinitialize
-let a = 10;
- console.log(a);
-  x = 15;
-console.log(a);
-// let can be reassigned but not reinitialize 
 
-// while using const 
-// const b = 7;
-// console.log(b);
-// b = 6;
+console.log(currentDate, currentMonth, currentYear)
 
-// console.log(a)
-// this will throw an error because const can't be reassigned and reinitialize
+{/** date in utc format */ }
+let dateInUTCFormat = date.toUTCString();
+console.log(dateInUTCFormat, "dateinUTCFormat");
 
-var c = 9;
-var c = 31;
-console.log(c)
-c = 50;
-// this will not throw any error
 
-//hoisting 
+{/** date formatting using INTL */ }
+let dateForInTl = new Intl.DateTimeFormat("en-IN").format(date);
+// let dateForInTl = new Intl.DateTimeFormat("en-US").format(date);
 
-// var and function show hosting but let and const due to not having global contex eg
-console.log(zz);
-var zz = 10;
-// output undefined ; 
+{/** in the INTL we can pass the options */ }
+const options = {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  timeZone: "Asia/Kolkata",
+};
+let dateOfInTl = new Intl.DateTimeFormat("en-IN", options).format(date);
+// {
+//   /** India format */
+// }
+console.log(dateOfInTl, "dateInIntl");
 
-// console.log(zzz)
-// let zzz = 11;
-// output refrence error 
+let currency = 12654
+let IntlCurrency = new Intl.NumberFormat("en-US", { currency: "INR", style: "currency", maximumSignificantDigits: 4 }).format(currency);
+console.log(IntlCurrency)
 
-// console.log(w);
-// const w = 87
-// output refrence error
 
-sum(7,8);
-function sum (a,b){
-    console.log( a+b)
+let number = 45678
+let currencyType = new Intl.NumberFormat("en-US", { currency: "USD", style: "currency", maximumSignificantDigits: 5 }).format(number);
+console.log(currencyType)
+
+class student {
+  constructor(name, age) {
+    this.name = name,
+      this.age = age
+
+  }
+  display() {
+    console.log(`${this.name} ${this.age}`)
+  }
 }
+let newOne = new student("sam", 22);
 
-//output 15 no error because of hosting 
+class student2 extends student {
+  numberOfStudent = 20
+  newStudent="kelly"
+}
+let newData = new student2("rishi", 21)
+console.log(newData.newStudent)
+  
+class cars{
+  constructor(name,year){
+    this.name=name
+    this.year=year
+  }
+  car(){
+    console.log(`${this.name}${this.year}`)
+  }
+}
+let newOneCar=new cars ("audi",2015)
+
+class car2 extends cars{
+  newcar="BMW"
+}
+  let newCar= new car2("alto",2016)
+  console.log(newCar.newCar)
