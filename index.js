aac9867982a6b2627dcc1348fda5454cc56a89a2
Nidhi Rana <nidhi2.netweb@gamil.com>
2024-02-29 14:35:40 +0530

    date-time-format

--- index.js
+++ index.js
@@ -48,27 +48,38 @@ class student {
   }
 }
 let newOne = new student("sam", 22);
+console.log(newOne)
+newOne.display()
+
+let rohan = new student("herry", 34)
+console.log(rohan)
+rohan.display()
 
 class student2 extends student {
   numberOfStudent = 20
-  newStudent="kelly"
+
 }
+
+
 let newData = new student2("rishi", 21)
-console.log(newData.newStudent)
-  
-class cars{
-  constructor(name,year){
-    this.name=name
-    this.year=year
+console.log(newData)
+newData.numberOfStudent
+
+class cars {
+  constructor(name, year) {
+    this.name = name
+    this.year = year
   }
-  car(){
+  car() {
     console.log(`${this.name}${this.year}`)
   }
 }
-let newOneCar=new cars ("audi",2015)
-
-class car2 extends cars{
-  newcar="BMW"
+let newOneCar = new cars("audi", 2015)
+console.log(newOneCar)
+newOneCar.car()
+class car2 extends cars {
+  oneCarMore = "BMW"
 }
-  let newCar= new car2("alto",2016)
-  console.log(newCar.newCar)
+let newCar = new car2("alto", 2016)
+console.log(newCar)
+newCar.oneCarMore
