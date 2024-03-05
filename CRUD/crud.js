ecb07495a5dec34ecdb24e8d3f8ffacaac28fb3e
vinaykumar <vinay1.netweb@gmail.com>
2024-03-05 18:47:29 +0530

    worked_on_CRUD_update_button

--- CRUD/crud.js
+++ CRUD/crud.js
@@ -1,130 +1,111 @@
-let btn = document.getElementById("button_click");
+    async function fetchNews() {
+        const url = "https://jsonplaceholder.typicode.com/users";
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
 
-//api call on Button Click
+    async function showOutput() {
+        const data = await fetchNews();
+        const container = document.querySelector(".container-item");
 
-const fetchData = async () => {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const result = await response.json();
-    getData(result);
-    console.log(result, "result");
-  } catch (error) {}
-};
+        data.forEach(item => {
+            const tr = document.createElement("tr");
+            tr.setAttribute("data-id", item.id);
+            
 
-//Delete Data from Api
+            const id = document.createElement("td");
+            id.textContent = item.id;
+            tr.appendChild(id);
 
-const DeleteData = async (userId) => {
-  try {
-    await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {
-      method: "DELETE",
-    });
-    console.log(`user with id ${userId} removed `);
-  } catch (error) {
-    console.log(error)
-  }
-};
+            const name = document.createElement("td");
+            name.textContent = item.name;
+            tr.appendChild(name);
 
-//Edit Data in api
+            const userName = document.createElement("td");
+            userName.textContent = item.username;
+            tr.appendChild(userName);
 
-const Edit_Data = async (userId, newEmail) => {
-  try {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/users/${userId}`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: newEmail,
-        }),
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
+            const email = document.createElement("td");
+            email.textContent = item.email;
+            tr.appendChild(email);
 
-//Post Data
+            const delete_action = document.createElement("td");
+            const delete_button = document.createElement("button");
+            delete_button.classList.add("btn");
+            delete_button.innerText = "Delete";
+            delete_button.addEventListener("click", function () {
+                deletePost(item.id, tr);
+                delete_button.innerHTML = "Deleted &#10004;";
+            });
+            delete_action.appendChild(delete_button);
 
-btn.addEventListener("click", fetchData);
+            const edit_button = document.createElement("button");
+            edit_button.classList.add("btn");
+            edit_button.innerText = "Update";
+            edit_button.setAttribute("data-bs-toggle", "modal");
+            edit_button.setAttribute("data-bs-target", "#exampleModal");
+            edit_button.addEventListener("click", function () {
+                populateModal(item.id, item.email, item.body);
+                console.log("hello")
+            });
+            delete_action.appendChild(edit_button);
 
-const getData = (result) => {
-  let container = document.getElementById("data_table");
+            tr.appendChild(delete_action);
 
-  result.forEach((value) => {
-    let row = document.createElement("tr");
+            container.appendChild(tr);
+        });
+    }
 
-    container.appendChild(row);
+    showOutput();
 
-    //for-useName
-    let userName = document.createElement("td");
-    userName.textContent = value.username;
-    row.appendChild(userName);
+    function populateModal(id, email,) {
+        document.getElementById("postId").value = id;
+        document.getElementById("email").value = email;
+    }
+    
+    async function saveChanges() {
+        const postId = document.getElementById("postId").value;
+        const newTitle = document.getElementById("email").value;
 
-    //for-name
-    let name = document.createElement("td");
-    name.textContent = value.name;
-    row.appendChild(name);
+        const url = `https://jsonplaceholder.typicode.com/users/${postId}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: newTitle,
+                // body: newBody
+            })
+        };
 
-    //for email
-    let email = document.createElement("td");
-    email.textContent = value.email;
-    row.appendChild(email);
+        try {
+            const response = await fetch(url, requestOptions);
+            if (response.ok) {
+                const updatedData = await response.json();
+                const email = document.querySelector(`tr[data-id="${postId}"] td:nth-child(4)`);
+                // const userName = document.querySelector(`tr[data-id="${postId}"] td:nth-child(4)`);
+                email.textContent = updatedData.email;
+                // $('#exampleModal').modal('hide');
+                
+                
+            } else {
+                throw new Error('Failed to update post.');
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    }
 
-    //Delete Button
-    let delete_button = document.createElement("button");
-    delete_button.textContent = "Delete";
-    delete_button.addEventListener("click", () => {
-      DeleteData(value.id);
-      row.remove();
-    });
-    let action = document.createElement("td");
-    action.appendChild(delete_button);
-    row.appendChild(action);
-
-    //Edit Button
-    let edit_btn = document.createElement("button");
-    edit_btn.textContent = "Edit";
-
-    edit_btn.addEventListener("click", () => {
-      Edit_Data(value.id, (value.email = "peter@gmail.com"));
-      // row.appendChild()
-    });
-
-    let edit_action = document.createElement("td");
-    edit_action.appendChild(edit_btn);
-    row.appendChild(edit_action);
-  });
-};
-
-
-//Post_Data 
-let data = document.getElementById("post_data");
-
-const Post_Data = async (email, userName) => {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        name: userName,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-data.addEventListener("click", () => {
-  const nameInput = document.getElementById("name").value;
-  const emailInput = document.getElementById("email").value;
-  Post_Data(emailInput, nameInput);
-});
+    function deletePost(id, row) {
+        const url = `https://jsonplaceholder.typicode.com/users/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(response => {
+                if (response.ok) {
+                    row.remove();
+                }
+            });
+    }
