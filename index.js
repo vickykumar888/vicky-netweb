4e43b9b95e6a8c4399c10ec1a625dc91c2b064ff
Nidhi Rana <nidhi2.netweb@gmail.com>
2024-03-05 15:19:58 +0530

    arrayOfobject

--- index.js
+++ index.js
@@ -15,27 +15,3 @@ arrayOfObject.map((item) => {
   container.appendChild(data)
   return item.text
 })
-
-const inputNumber = document.getElementById("input")
-const tableNumber = document.getElementById("table")
-
-inputNumber.addEventListener("input", () => {
-  const number = parseInt(inputNumber.value)
-
-  if (!isNaN(number)) {
-    generateTable(number)
-  } else {
-    tableNumber.innerHTML = ""
-  }
-})
-
-
-function generateTable(number) {
-  // console.log("==>",number)
-  let tableHtml = "<table>"
-  for (let i = 0; i <= 10; i++) {
-    tableHtml += `<tr><td>${number}*${i}</td><td>=</td><td>${number * i}</td></tr>`
-  }
-  tableHtml += "</table>"
-  tableNumber.innerHTML = tableHtml
-}
