d1d91ef56b10a6fc3bdc642832acd8749f97e591
Nidhi Rana <nidhi2.netweb@gmail.com>
2024-03-06 16:07:36 +0530

    assignmentofArray-andTable

---
+++ table.js
@@ -0,0 +1,24 @@
+
+const inputNumber = document.getElementById("input")
+const tableNumber = document.getElementById("table")
+
+inputNumber.addEventListener("input", () => {
+  const number = parseInt(inputNumber.value)
+
+  if (!isNaN(number) && (number > 0)) {
+    generateTable(number)
+  } else {
+    tableNumber.innerHTML = ""
+  }
+})
+
+
+function generateTable(number) {
+  // console.log("==>",number)
+  let tableHtml = "<table>"
+  for (let i = 1; i <= 13; i++) {
+    tableHtml += `<tr><td>${number}*${i}</td><td>=</td><td>${number * i}</td></tr>`
+  }
+  tableHtml += "</table>"
+  tableNumber.innerHTML = tableHtml
+}
