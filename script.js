a4ab957d603ad3e212f0a61fd1503611f296f08d
poojachauhan1500 <pc087552@gmail.com>
2024-03-07 16:03:49 +0530

    tour task ui and functionality

---
+++ script.js
@@ -0,0 +1,109 @@
+let data = [
+    {
+        img:"https://images.pexels.com/photos/460740/pexels-photo-460740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",price:"$356", heading:"Best Of Paris In 7 Days Tour",  para:"Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!"
+    },
+    {
+        img:"https://images.pexels.com/photos/631401/pexels-photo-631401.jpeg",heading:"Best Of Ireland In 14 Days Tour",price:"$356", para:"Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites, charming towns, music-filled pubs, and seaside getaways — including Kinsale, the Dingle Peninsula, the Cliffs of Moher, the Aran Islands, Galway, Connemara, Giant's Causeway, and the compelling city of Belfast. All along the way, Rick's guides will share their stories to draw you in to the Emerald Isle, and the friendliness of the people will surely steal your heart. Join us for the Best of Ireland in 14 Days!"
+    },
+    {
+        img:"https://images.pexels.com/photos/2225442/pexels-photo-2225442.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",price:"$356" ,heading:"Best Of Salzburg & Vienna In 8 Days Tour",para:"Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich, Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna — with cozy villages and alpine vistas all along the way. Join us for the Best of Munich, Salzburg & Vienna in 8 Days!"
+    },
+    {
+        img:"https://images.pexels.com/photos/2225442/pexels-photo-2225442.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",price:"$356",heading:"Best Of Rome In 7 Days Tour",para:"Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of ancient Rome's Colosseum, Forum, Pantheon, and nearby Ostia Antica. From the Renaissance and Baroque eras, you'll marvel at St. Peter's Basilica, the Vatican Museums, Sistine Chapel, and Borghese Gallery. You'll also enjoy today's Rome, with neighborhood walking tours, memorable restaurants, and time to explore on your own. Join us for the Best of Rome in 7 Days!"
+    },
+    {
+        img:"https://images.pexels.com/photos/4969843/pexels-photo-4969843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",price:"$356",heading:"Best Of Poland In 10 Days Tour",para:"Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!"
+    }
+
+]
+
+let result = data.map((output)=>{
+    return output
+})
+// console.log(result);
+
+function showData(data) {
+    return `
+        <div class="item">
+            <img class="image" src="${data.img}" alt="">
+            <h3>${data.price}</h3>
+            <div class="content">
+            <h2>${data.heading}</h2>
+            <p id="mainBtn">${data.para} <button class="para-btn">Read more</button></p>
+            <button class="btn">Not interested</button>
+            </div>
+        </div>
+    `;
+}
+
+
+
+function showAllData(){
+    let container = document.querySelector(".container")
+    let content = "";
+    data.forEach((conclusion)=>{
+        content+=showData(conclusion);
+    })
+    container.insertAdjacentHTML("beforeend", content);
+    //   console.log(container.innerHTML);
+
+
+      document.addEventListener("DOMContentLoaded", function() {
+        let readMoreBtns = document.querySelectorAll(".para-btn");
+        let mainBtn = document.querySelector("#mainBtn");
+    
+        readMoreBtns.forEach(button => {
+            button.addEventListener("click", function() {
+                console.log("clickd");
+                let parent = button.parentNode;
+                console.log("Parent node:", parent);
+                 console.log("Parent class:", parent.classList.value);
+                console.log("Parent id:", parent.id);
+                mainBtn.classList.toggle("expanded");
+                // console.log("Main button classList:", mainBtn.classList);
+    
+                if (mainBtn.classList.contains("expanded")) {
+                    button.textContent = "Read less";
+                    // mainBtn.style.overflow="hidden"
+                    mainBtn.style.height="100%"
+
+                    
+                } else {
+                    button.textContent = "Read more";
+                    // mainBtn.style.overflow="auto"
+                    mainBtn.style.height="20%"
+                     
+                }
+            });
+        });
+    });
+    
+
+
+
+
+    
+    let buttons = document.querySelectorAll(".btn");
+    let totalTours = buttons.length; 
+    
+    buttons.forEach(button => {
+        button.addEventListener("click", function() {
+            button.parentNode.parentNode.remove();
+            // console.log("Button clicked");
+    
+            totalTours--; 
+    
+            if (totalTours === 0) {
+                let heading = document.querySelector(".heading");
+                heading.innerHTML = "0 tours left";
+              
+            }
+        });
+    });
+}
+
+
+
+
+
+showAllData();
